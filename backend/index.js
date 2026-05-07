6b4290e9eb1dc6da7520ff7532ab681bfcbbf2e3
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-07 20:38:50 +0530

    update the index.js file

--- backend/index.js
+++ backend/index.js
@@ -115,7 +115,7 @@ app.get('/' , (req  ,res)=>{
 })
 
 // React app catch-all
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
